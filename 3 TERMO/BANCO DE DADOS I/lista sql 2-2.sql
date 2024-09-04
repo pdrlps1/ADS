a2b0fddf4f3a728aa51fc2b1630c371883262b80
pdrlps1 <pedro.otavio-12@outlook.com>
2024-09-04 20:02:24 -0300

    aula-02-09

--- 3 TERMO/BANCO DE DADOS I/lista sql 2-2.sql
+++ 3 TERMO/BANCO DE DADOS I/lista sql 2-2.sql
@@ -41,3 +41,45 @@ values
 ("Josefina Varsóvia",31,"1996-10-11","Secretária",1200.00,4),
 ("Lepeka Tossi",26,"2000-12-12","Secretária",1100.00,3),
 ("Pacífico Gerra",31,"1997-01-01","Vendedor",2500.00,2);
+
+update empregado
+set des_cargo = "Engenheiro Mestre"
+where des_nom_emp = "João Gonzo";
+
+update empregado
+set val_salar_emp = 3200.00
+where des_idade_emp >= 40;
+
+update empregado
+set val_salar_emp = 2300.00
+where des_cargo = "Copeira";
+
+update empregado
+set des_nom_emp = "João Gonzzo"
+where des_nom_emp = "João Gonzo";
+
+update empregado
+set des_idade_emp = 37
+where des_nom_emp = "João Gonzzo";
+
+update depto
+set val_orcam_depto = 2500000.00
+where val_orcam_depto >= 2000000.00;
+
+update empregado
+set val_salar_emp = 2100.00
+where dat_adm_emp = "2000-12-12";
+
+update depto
+set des_local_depto = "Prédio Central"
+where nom_depto = "Diretoria";
+
+update empregado
+set des_idade_emp = 27
+where dat_adm_emp = "2000-12-12";
+
+alter table empregado
+change des_cargo des_cargo_emp varchar (20);
+
+alter table empregado
+modify des_nom_emp varchar(60);
